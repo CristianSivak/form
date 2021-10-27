66580fc30bfdff3581703eacc30de7e31bd649cc
Cristian Sivak <68347600+CristianSivak@users.noreply.github.com>
2021-10-27 18:42:03 -0300

    files

---
+++ script.js
@@ -0,0 +1,7 @@
+const alertDatos = (e) =>{
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
+    return alert(`Name: ${name}, Email: ${email}, Subject: ${subject}, Message: ${message} `)
+}
